c8f10d4d9e7c82c3f9faf73486e5888af7e5f661
Cahzello <rizkyrasya.m@gmail.com>
2024-03-18 09:58:55 +0700

    feat: edit conditional rendering UI dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,11 +5,11 @@
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
         <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-                class="fas fa-download fa-sm text-whphp ite-50"></i> Genera te Report</a>
+                class="fas fa-download fa-sm text-whphp ite-50"></i> Generate Report</a>
     </div>
 
     <!-- Content Row -->
-    <div class="row">
+    {{-- <div class="row">
 
         <!-- Earnings (Monthly) Card Example -->
         <div class="col-xl-3 col-md-6 mb-4">
@@ -30,74 +30,12 @@
             </div>
         </div>
 
-        <!-- Earnings (Monthly) Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-success shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                Earnings (Annual)</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Earnings (Monthly) Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-info shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks
-                            </div>
-                            <div class="row no-gutters align-items-center">
-                                <div class="col-auto">
-                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
-                                </div>
-                                <div class="col">
-                                    <div class="progress progress-sm mr-2">
-                                        <div class="progress-bar bg-info" role="progressbar" style="width: 50%"
-                                            aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Pending Requests Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-warning shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                Pending Requests</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-comments fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+        
+    </div> --}}
 
     <!-- Content -->
     <div class="row">
-        <div class="col-lg-6 mb-4">
+        <div class="col-lg-6 mb-2">
 
             <!-- Approach -->
             <div class="card shadow mb-4">
@@ -105,9 +43,39 @@
                     <h6 class="m-0 font-weight-bold text-primary">Latest Item Created</h6>
                 </div>
                 <div class="card-body">
-                    <p>Item Name:  {{ $data['itemData']->name }}</p>
-                    <p>Date Item Created:  {{ $data['itemData']->created_at->setTimeZone('Asia/Jakarta')->toDayDateTimeString() }}</p>
-                    <p>Date Item Updated:  {{ $data['itemData']->updated_at->setTimeZone('Asia/Jakarta')->toDayDateTimeString() }}</p>
+                    @if ($data['itemData'] == null)
+                        <p>No Data Created</p>
+                        <a href="{{route('items.create')}}" class="btn btn-primary">Create Item</a>
+                    @else
+                        <p>Item Name: {{ $data['itemData']->name }}</p>
+                        <p>Date Item Created:
+                            {{ $data['itemData']->created_at->setTimeZone('Asia/Jakarta')->toDayDateTimeString() }}</p>
+                        <p>Date Item Updated:
+                            {{ $data['itemData']->updated_at->setTimeZone('Asia/Jakarta')->toDayDateTimeString() }}</p>
+                    @endif
+                </div>
+            </div>
+
+        </div>
+
+        <div class="col-lg-6 mb-2">
+
+            <!-- Approach -->
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">Latest Category Created</h6>
+                </div>
+                <div class="card-body">
+                    @if ($data['categoryData'] == null)
+                        <p>No Data Created</p>
+                        <a href="{{route('category.create')}}" class="btn btn-primary">Create Category</a>
+                    @else
+                        <p>Item Name: {{ $data['categoryData']->name }}</p>
+                        <p>Date Item Created:
+                            {{ $data['categoryData']->created_at->setTimeZone('Asia/Jakarta')->toDayDateTimeString() }}</p>
+                        <p>Date Item Updated:
+                            {{ $data['categoryData']->updated_at->setTimeZone('Asia/Jakarta')->toDayDateTimeString() }}</p>
+                    @endif
                 </div>
             </div>
 
@@ -118,16 +86,22 @@
             <!-- Approach -->
             <div class="card shadow mb-4">
                 <div class="card-header py-3">
-                    <h6 class="m-0 font-weight-bold text-primary">Latest Category Created</h6>
+                    <h6 class="m-0 font-weight-bold text-primary">Latest Customer Inputted</h6>
                 </div>
                 <div class="card-body">
-                    <p>Category Name:  {{ $data['categoryData']->name }}</p>
-                    <p>Date Category Created:  {{ $data['categoryData']->created_at->setTimeZone('Asia/Jakarta')->toDayDateTimeString() }}</p>
-                    <p>Date Category Updated:  {{ $data['categoryData']->updated_at->setTimeZone('Asia/Jakarta')->toDayDateTimeString() }}</p>
+                    @if ($data['customerData'] == null)
+                        <p>No Data Inputted</p>
+                        <a href="{{route('customer.create')}}" class="btn btn-primary">Input Customer</a>
+                    @else
+                        <p>Item Name: {{ $data['customerData']->name }}</p>
+                        <p>Date Item Created:
+                            {{ $data['customerData']->created_at->setTimeZone('Asia/Jakarta')->toDayDateTimeString() }}</p>
+                        <p>Date Item Updated:
+                            {{ $data['customerData']->updated_at->setTimeZone('Asia/Jakarta')->toDayDateTimeString() }}</p>
+                    @endif
                 </div>
             </div>
 
         </div>
     </div>
-    
 @endsection
