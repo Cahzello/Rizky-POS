5107c4c57f93403d59c58ab7c7e6db4459a2a6f7
Cahzello <rizkyrasya.m@gmail.com>
2024-03-21 19:31:32 +0700

    fix: edit web route

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::middleware('guest')->group(function () {
+    Route::get('/', [AuthController::class, 'viewLogin']);
+
     Route::get('/login', [AuthController::class, 'viewLogin'])->name('login');
     
     Route::post('/login', [AuthController::class, 'login']);
